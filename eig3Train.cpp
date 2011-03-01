9b4621e110e37e59c6853f2f598114016ce2b4dc
david <david>
2011-03-01 12:15:48 +0000

    Added argv processing.

--- eig3Train.cpp
+++ eig3Train.cpp
@@ -1,8 +1,19 @@
+#include <iostream>
 #include "Database.h"
 
 int main(int argc, char *argv[]) {
-  //const std::string dir = "/afs/l2f/home/ferreira/FaceRec/ImageVault/train";
-  const std::string dir = "/afs/l2f/home/ferreira/face-recognition/MyTrainDatabase";
+  //const std::string dir = "/afs/l2f/home/ferreira/face-recognition/ImageVault/train";
+  std::string dir;
+
+  if (argc == 1) {
+    dir = "/afs/l2f/home/ferreira/face-recognition/MyTrainDatabase";
+  } else if (argc == 2) {
+    dir = argv[1];
+  } else {
+    std::cerr << "Usage: " << argv[0] << " [ directory_name ]" << std::endl;
+    exit(1);
+  }
+
   efj::Database db(dir);
   db.compute_eigenfaces();
   db.write("batata.dat");
